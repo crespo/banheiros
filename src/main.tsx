82bb4e4697efd1c2e4c917eba378670a8dc895f4
raul crespo <raulxcrespo@gmail.com>
2026-08-02 16:39:45 -0300

    style: add organic design tokens and self-hosted fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "@fontsource/caprasimo/400.css";
+import "@fontsource/figtree/400.css";
+import "@fontsource/figtree/600.css";
+import "@fontsource/figtree/700.css";
+import "./styles/tokens.css";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
