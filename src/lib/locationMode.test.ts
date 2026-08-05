183138d2683dff71a2ea074bb07f4242ed89c416
raul crespo <raulxcrespo@gmail.com>
2026-08-05 18:52:47 -0300

    test: resolveLocationMode returns approximate mode for non-success outcomes

--- src/lib/locationMode.test.ts
+++ src/lib/locationMode.test.ts
@@ -4,3 +4,7 @@ import { resolveLocationMode } from "./locationMode";
 test('resolveLocationMode("success") returns the precise mode', () => {
   expect(resolveLocationMode("success")).toBe("precise");
 });
+
+test('resolveLocationMode("error") returns the approximate mode', () => {
+  expect(resolveLocationMode("error")).toBe("approximate");
+});
